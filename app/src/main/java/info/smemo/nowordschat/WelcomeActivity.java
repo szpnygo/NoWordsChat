0b953a84ee86e206cfe67ffb289dede10c5f49a0
suzhenpeng <280097765@qq.com>
2016-12-02 00:18:28 +0800

    构建toolbar

--- app/src/main/java/info/smemo/nowordschat/WelcomeActivity.java
+++ app/src/main/java/info/smemo/nowordschat/WelcomeActivity.java
@@ -1,7 +1,9 @@
 package info.smemo.nowordschat;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import info.smemo.nowordschat.activity.MainActivity;
 import info.smemo.nowordschat.base.BaseActivity;
 
 public class WelcomeActivity extends BaseActivity {
@@ -9,6 +11,8 @@ public class WelcomeActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
