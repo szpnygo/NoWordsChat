24ce1f67253601c77ac96baa9bd58baff70c9395
neosu <280097765@qq.com>
2017-01-03 16:04:51 +0800

    第一层朋友圈数据赋值

--- app/src/main/java/info/smemo/nowordschat/activity/FindActivity.java
+++ app/src/main/java/info/smemo/nowordschat/activity/FindActivity.java
@@ -6,13 +6,17 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.ArrayList;
+
 import info.smemo.nowordschat.R;
 import info.smemo.nowordschat.adapter.FindAdapter;
 import info.smemo.nowordschat.base.BaseCompatActivity;
+import info.smemo.nowordschat.bean.FindBean;
 
 public class FindActivity extends BaseCompatActivity {
 
     private FindAdapter findAdapter;
+    private ArrayList<FindBean> mFindBeanArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +32,63 @@ public class FindActivity extends BaseCompatActivity {
     }
 
     private void initAdapter() {
-        findAdapter = new FindAdapter();
+        FindBean findBean = new FindBean();
+        findBean.imgUri.add("res:///" + R.drawable.user_logo);
+        findBean.userLogo = "res:///" + R.drawable.user_logo;
+        findBean.username = "朋友圈昵称";
+        findBean.favorites = 5;
+        findBean.comments = 9;
+
+        FindBean findBean1 = new FindBean();
+        findBean1.imgUri.add("res:///" + R.drawable.user_logo);
+        findBean1.imgUri.add("res:///" + R.drawable.user_logo);
+        findBean1.userLogo = "res:///" + R.drawable.user_logo;
+        findBean1.username = "朋友圈昵称";
+        findBean1.favorites = 5;
+        findBean1.comments = 9;
+
+
+        FindBean findBean2 = new FindBean();
+        findBean2.imgUri.add("res:///" + R.drawable.user_logo);
+        findBean2.userLogo = "res:///" + R.drawable.user_logo;
+        findBean2.username = "朋友圈昵称";
+        findBean2.favorites = 5;
+        findBean2.comments = 9;
+
+
+        FindBean findBean3 = new FindBean();
+        findBean3.imgUri.add("res:///" + R.drawable.user_logo);
+        findBean3.imgUri.add("res:///" + R.drawable.user_logo);
+        findBean3.imgUri.add("res:///" + R.drawable.user_logo);
+        findBean3.imgUri.add("res:///" + R.drawable.user_logo);
+        findBean3.userLogo = "res:///" + R.drawable.user_logo;
+        findBean3.username = "朋友圈昵称";
+        findBean3.favorites = 5;
+        findBean3.comments = 9;
+
+        FindBean findBean4 = new FindBean();
+        findBean4.imgUri.add("res:///" + R.drawable.user_logo);
+        findBean4.imgUri.add("res:///" + R.drawable.user_logo);
+        findBean4.imgUri.add("res:///" + R.drawable.user_logo);
+        findBean4.imgUri.add("res:///" + R.drawable.user_logo);
+        findBean4.imgUri.add("res:///" + R.drawable.user_logo);
+        findBean4.imgUri.add("res:///" + R.drawable.user_logo);
+        findBean4.imgUri.add("res:///" + R.drawable.user_logo);
+        findBean4.imgUri.add("res:///" + R.drawable.user_logo);
+        findBean4.imgUri.add("res:///" + R.drawable.user_logo);
+        findBean4.userLogo = "res:///" + R.drawable.user_logo;
+        findBean4.username = "朋友圈昵称";
+        findBean4.favorites = 5;
+        findBean4.comments = 9;
+
+        mFindBeanArrayList = new ArrayList<>();
+        mFindBeanArrayList.add(findBean);
+        mFindBeanArrayList.add(findBean1);
+        mFindBeanArrayList.add(findBean2);
+        mFindBeanArrayList.add(findBean3);
+        mFindBeanArrayList.add(findBean4);
+
+        findAdapter = new FindAdapter(this, mFindBeanArrayList);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.find_list);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
--- app/src/main/java/info/smemo/nowordschat/adapter/FindAdapter.java
+++ app/src/main/java/info/smemo/nowordschat/adapter/FindAdapter.java
@@ -1,35 +1,78 @@
 package info.smemo.nowordschat.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.databinding.library.baseAdapters.BR;
+
+import java.util.ArrayList;
+
+import info.smemo.nbaseaction.base.NBaseViewHolder;
 import info.smemo.nowordschat.R;
+import info.smemo.nowordschat.bean.FindBean;
 
 public class FindAdapter extends RecyclerView.Adapter {
 
+    public static final int FIND_TYPE_ONE_PIC = 1;
+    public static final int FIND_TYPE_MORE_PICS = 2;
+
+    private Context mContext;
+
+    private ArrayList<FindBean> findBeanList;
+
+    public FindAdapter(Context context) {
+        this.mContext = context;
+    }
+
+    public FindAdapter(Context context, ArrayList<FindBean> findBeanList) {
+        mContext = context;
+        this.findBeanList = findBeanList;
+    }
+
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_find, null);
-        view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        return new ItemHolder(view);
+        if (viewType == FIND_TYPE_MORE_PICS) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_find_pics, null);
+            view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            return new ImageHolder(view, viewType);
+        } else {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_find_pic, null);
+            view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            return new ImageHolder(view, viewType);
+        }
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+        if (getItemViewType(position) == FIND_TYPE_MORE_PICS || getItemViewType(position) == FIND_TYPE_ONE_PIC) {
+            ImageHolder imageHolder = (ImageHolder) holder;
+            imageHolder.getBinding().setVariable(BR.bean, findBeanList.get(position));
+            imageHolder.getBinding().executePendingBindings();
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 5;
+        return findBeanList.size();
     }
 
-    class ItemHolder extends RecyclerView.ViewHolder {
+    @Override
+    public int getItemViewType(int position) {
+        return findBeanList.get(position).imgUri.size() > 1 ? FIND_TYPE_MORE_PICS : FIND_TYPE_ONE_PIC;
+    }
 
-        public ItemHolder(View itemView) {
+    //图片类型
+    class ImageHolder extends NBaseViewHolder {
+
+        private int type;
+
+        public ImageHolder(View itemView, int type) {
             super(itemView);
+            this.type = type;
         }
+
     }
 }
