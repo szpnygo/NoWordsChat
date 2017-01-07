b0d4b362c617cd286f6b3fc71538e1f1b2b2a084
neosu <280097765@qq.com>
2017-01-07 17:43:24 +0800

    更换LeanCloud key

--- leancloud/src/main/java/info/smemo/nowordschat/leancloud/LeanCloudConfig.java
+++ leancloud/src/main/java/info/smemo/nowordschat/leancloud/LeanCloudConfig.java
@@ -2,6 +2,6 @@ package info.smemo.nowordschat.leancloud;
 
 public interface LeanCloudConfig {
 
-    String APP_ID = "7qaq9T8tQucLNvLlXoz9X5pK-9Nh9j0Va";
-    String APP_KEY = "uMxraL5mmfHmxMo9VAiSjBmj";
+    String APP_ID = "7Yix1eUiB5xk3DGCDq8kjwuS-9Nh9j0Va";
+    String APP_KEY = "glohvCSKvGkMMrErI6rQc5CL";
 }
