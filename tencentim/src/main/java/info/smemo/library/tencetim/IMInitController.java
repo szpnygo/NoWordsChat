a89637c5857c4eb51b5429e841fd4cd8866723eb
suzhenpeng <280097765@qq.com>
2017-01-15 23:47:20 +0800

    添加一些IMSDK初始化

--- tencentim/src/main/java/info/smemo/library/tencetim/IMInitController.java
+++ tencentim/src/main/java/info/smemo/library/tencetim/IMInitController.java
@@ -2,6 +2,7 @@ package info.smemo.library.tencetim;
 
 import android.content.Context;
 
+import com.tencent.TIMLogLevel;
 import com.tencent.TIMManager;
 
 /**
@@ -13,6 +14,17 @@ public class IMInitController implements IMConstant {
 
     public static void init(Context context) {
         TIMManager.getInstance().init(context);
+        //开启本地储存
+        TIMManager.getInstance().enableFriendshipStorage(true);
+        //启用群资料存储
+        TIMManager.getInstance().enableFriendshipStorage(true);
+        //设置日志级别
+        TIMManager.getInstance().setLogLevel(TIMLogLevel.INFO);
+        //开启已读回执
+        TIMManager.getInstance().enableReadReceipt();
+        //禁用自动上报
+        TIMManager.getInstance().disableAutoReport();
+
         IMUserController.getInstance().init(context);
         IMLoginController.getInstance().init(context);
     }
