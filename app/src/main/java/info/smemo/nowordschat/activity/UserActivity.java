afac3c61e8e77bc79ff6bc9e2812142fb53866f1
neosu <280097765@qq.com>
2017-02-03 11:51:58 +0800

    修复个人信息页面

--- app/src/main/java/info/smemo/nowordschat/activity/UserActivity.java
+++ app/src/main/java/info/smemo/nowordschat/activity/UserActivity.java
@@ -43,11 +43,7 @@ public class UserActivity extends BaseCompatActivity implements UserContract.Vie
 
         presenter = new UserPresenter(this);
         binding.setPresenter(presenter);
-    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         initFindAdapter();
     }
 
